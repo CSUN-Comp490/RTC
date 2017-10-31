805ae1c932a345302bae6e8db0cd21ed60ef3401
Benjamin Overton <benjamin.overton.467@my.csun.edu>
2017-10-31 00:24:48 -0700

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(process.env.PORT || 8081);
+console.log('Listening on localhost:8081');
