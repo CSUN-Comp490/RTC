a22acda7b4c410622f053ed1c6327906dee48601
Thomas Leavy <trleavy@Thomass-iMac.local>
2017-11-13 15:24:54 -0800

    socket stuff. doesnt work yet

--- server/src/app.js
+++ server/src/app.js
@@ -6,13 +6,23 @@ const {sequelize} = require('./models')
 const config = require('./config/config')
 
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
 require('./routes')(app)
 
+
 sequelize.sync({force: false}).then(() => {
+    io.on('connection', function (socket) {
+        socket.on('chat message', function(msg){
+            io.emit('chat message', msg);
+          })
+    })
+
     app.listen(config.port)
     console.log(`Server started on port ${config.port}`)
+    
 })
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,14 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+const io = require('socket.io')
+const path = require('path')
 
 module.exports = (app) => {
     app.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register)
 
     app.post('/login', AuthenticationController.login)
+
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/index.html')
+    })
 }
