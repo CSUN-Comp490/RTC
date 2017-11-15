c377a5fab8be6b6b431f9f175771356c03d129bc
shortaflip <kevin_enario@yahoo.com>
2017-11-15 13:10:14 -0800

    changes to route captionist class page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Register from '@/components/Register'
-import Login from '@/components/Login'
+import CaptionistClassPage from '@/components/CaptionistClassPage'
 
 Vue.use(Router)
 
@@ -10,18 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'root',
+      name: 'Hello',
       component: HelloWorld
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
     },
     {
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/captionistclass',
+      name: 'CaptionistClassPage',
+      component: CaptionistClassPage
     }
   ]
 })
