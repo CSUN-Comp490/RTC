06ef71b100cfd87a1be6d78222cc45c97a5bb62f
shortaflip <kevin_enario@yahoo.com>
2017-11-15 12:54:35 -0800

    added code to enable bootstrap-vue.js

--- client/src/main.js
+++ client/src/main.js
@@ -3,13 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vuetify from 'vuetify'
-// import { sync } from 'vuex-router-sync'
-import 'vuetify/dist/vuetify.min.css'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
-
-Vue.use(Vuetify)
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
