4ef6e940aaa5bc9926327c62443020a9d31ead7e
mrci18 <charles.ibo.247@my.csun.edu>
2017-12-13 08:35:03 -0800

    Update routes.js

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,4 +5,9 @@ module.exports = (app) => {
   app.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register)
 
   app.post('/login', AuthenticationController.login)
+  
+   //move index to client at some point
+    app.get('/session', function (req, res) {
+        res.sendFile(__dirname + '/index.html')
+    })
 }
