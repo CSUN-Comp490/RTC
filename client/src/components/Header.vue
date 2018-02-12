afd6fcf958d7de316842775398a5db0a7ebe51c5
aprilLima <paperlima@gmail.com>
2018-02-11 20:40:09 -0800

    Made hamburger menu for phone view

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,31 +1,52 @@
 <template>
-  <nav class="navbar fixed-top navbar-collapse varela">
-    <div class="navbar-brand col-sm-2 col-sm-offset-1 home nav-item">
-      <span v-on:click="navigateTo({name: 'root'})">Retica</span>
+
+  <!--TODO:
+      how to glyphicons
+      how to images
+      float .link right?
+      -->
+
+  <nav class="navbar navbar-toggleable-sm navbar-inverse bg-inverse col-xs-12" style="background-color: #0a1d3b;">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!--TODO: Retica brand logo here-->
+    <button class="navbar-brand nav-item col-sm-offset-1">
+      <span class="home" v-on:click="navigateTo({name: 'root'})">Retica</span>
+    </button>
+
+    <div class="collapse navbar-collapse container" id="navbarSupportedContent">
+
+      <ul class="navbar-nav mx-auto ">
+
+        <li class="nav-item">
+          <button v-on:click="navigateTo({name: 'login'})" v-if="!$store.state.isUserLoggedIn" flat to="login" class="nav-item link">
+            <!--TODO: Glyphicon for login?-->
+            Log In
+          </button>
+        </li>
+
+        <li class="nav-item">
+          <button v-on:click="navigateTo({name: 'register'})" v-if="!$store.state.isUserLoggedIn" flat to="register" class="nav-item link">
+            <!--TODO: glyphicon for register? -->
+            Register
+          </button>
+        </li>
+
+        <li class="nav-item pull-right">
+          <button v-if="$store.state.isUserLoggedIn" flat @click="logout" class="nav-item link">
+            <!--TODO: glyphicon for logout? -->
+            Log Out
+          </button>
+        </li>
+
+      </ul>
     </div>
-
-    <!--<v-spacer></v-spacer>-->
-
-    <div class="">
-      <v-btn v-if="!$store.state.isUserLoggedIn" flat router to="login" class="nav-item">
-        Log In
-      </v-btn>
-
-      <span class="nav-divider"> | </span>
-
-      <v-btn v-if="!$store.state.isUserLoggedIn" flat router to="register" class="nav-item">
-        Register
-      </v-btn>
-
-
-      <v-btn v-if="$store.state.isUserLoggedIn" flat @click="logout" class="nav-item ">
-        Log Out
-      </v-btn>
-
-    </div>
-
   </nav>
 </template>
+
+
 
 <script>
   export default{
@@ -45,16 +66,19 @@
   }
 </script>
 
+
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .home{
-    cursor: pointer;
+    padding: 5px 10px;
   }
   .home:hover{
-    color: #000;
+    background-color: #3373ed;
+    border-radius: 10px;
   }
   nav {
-    display: inline;
+    /*display: inline;*/
     position: fixed;
     top: 0;
     border-radius: 0px 0px 25px 25px;
@@ -65,11 +89,18 @@
   }
   .nav-item {
     color: #ffffff;
+    padding: 5px 10px;
   }
-  .nav-divider {
-    color: #fff;
-    font-family: garamond;
-    font-size: 2em;
-    font-weight: 100;
+  .navbar-toggler {
+    /*background-color: white;*/
+    padding: 5px;
+    border-radius: 5px;
+  }
+  .link {
+    font-size: 1.2em;
+  }
+  .link:hover {
+    background-color: #3373ed;
+    border-radius: 5px;
   }
 </style>
